dd51582394d3731617fbbd43d27044f651b81fff
abrahams sarmiento <a.sarmientoluna@uandresbello.edu>
2024-04-22 20:11:52 -0400

    Vista Iniciar Sesion

---
+++ src/pages/Iniciarsesion.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, View } from 'react-native';
+import { FormContainer } from '../components/form-container';
+import { FormTitle } from '../components/form-title';
+import { Field } from '../components/field';
+import { Label } from '../components/label';
+import { CustomInput } from '../components/custom-input';
+import { CustomButton } from '../components/custom-button';
+
+
+export const Iniciarsesion = () => (
+    <View>
+      <FormContainer>
+        <FormTitle>Iniciar Sesión</FormTitle>
+  
+        <Field>
+          <Label>Correo Electronico</Label>
+          <CustomInput placeholder="Audifono@hotmail.com" />
+        </Field>
+        <Field>
+          <Label>Contraseña</Label>
+          <CustomInput placeholder="Audifono1234...." />
+        </Field>
+        <View style={styles.buttonContainer}>
+          <CustomButton title="Iniciar Sesión" />
+        </View>
+      </FormContainer>
+    </View>
+  );
+  
+  const styles = StyleSheet.create({
+    buttonContainer: {
+      marginTop: 24,
+    },
+  });
